a4ca5d5281631e09c6c0952f355eeb4256a67602
Johan Ström <johan@stromnet.se>
2014-09-30 12:17:42 +0200

    Bootstrap

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+
+if [ -L $0 ] ; then
+	DIR=$(dirname $(readlink -f $0))
+else
+	DIR=$(dirname $0)
+fi 
+ABSDIR=$(cd "$DIR"; pwd)
+
 bootstrap() {
 	FILE=$1
 	CONTENT=$2
@@ -7,6 +15,7 @@ bootstrap() {
 	if [ ! ~/${FILE} ]; then
 		echo $CONTENT > ~/${FILE}
 	else
+		echo 
 		echo "Ensure ~/${FILE} contains the following: "
 		echo "--------"
 		echo $CONTENT
@@ -14,6 +23,11 @@ bootstrap() {
 		echo
 	fi
 }
+
+if [ ! -L ~/.dotfiles ]; then
+	echo "Setting up link ~/.dotfiles to ${ABSDIR}"
+	ln -s ${ABSDIR} ~/.dotfiles
+fi
 
 bootstrap .zshrc "source ~/.dotfiles/.zshrc-global"
 bootstrap .vimrc "source ~/.dotfiles/.vimrc-global"
